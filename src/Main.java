234799f51811f86d8dee82037c9290a2c9c30387
lada <nicklada94@gmail.com>
2020-03-25 00:55:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 1000;
+        int coefficient = 20;
+        long bonus = price / coefficient;
+        System.out.println(bonus);
+    }
+}
